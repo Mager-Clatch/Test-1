3f5fc6fb8d8a9ecab9ea7c51e01b2cef5b630254
Mager-Clatch <112255672+Mager-Clatch@users.noreply.github.com>
2022-08-27 15:11:30 +0300

    Delete #app4 directory

--- #app4/js/script.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-
-    $('.start__image').slick({
-        prevArrow: '<img class="arrow arrow-left" src="img/arrow-1.svg" alt="">',
-        nextArrow: '<img class="arrow arrow-right" src="img/arrow-2.svg" alt="">',
-    });
-    
-});
